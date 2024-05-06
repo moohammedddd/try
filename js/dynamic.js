6944baf79f8f4a8022f4c38607bbc3c3c0250215
moohammedddd <midorafat115566@gmail.com>
2024-05-07 01:08:21 +0300

    try

---
+++ js/dynamic.js
@@ -0,0 +1,12 @@
+//792
+let lect = document.getElementById('lec')
+lect.addEventListener('click',function(){
+    window.scroll(0,800)})
+
+
+
+let list = document.getElementById('list')
+list.addEventListener('click',function(){
+    window.location.href = "list.html";
+})
+  
